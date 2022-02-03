cc00b38b8a9f89ab08395ee691664ab589f8daf0
ToKu404 <phantom26isn@gmail.com>
2022-02-03 23:54:54 +0800

    viral advertising

--- hacker_rank/viral_advertising/Solution.java
+++ hacker_rank/viral_advertising/Solution.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 class Solution {
     public static void main(String[] args) {
-        
+        Scanner inp = new Scanner(System.in);
+        int day = inp.nextInt();
+        viralAdverstising(day);
+        inp.close();
+    }
+
+    private static void viralAdverstising(int day) {
+        int shared = 5;
+        int liked = 2;
+        int cum = 2;
+        for (int i = 1; i < day; i++) {
+            shared = liked * 3;
+            liked = shared / 2;
+            cum += liked;
+        }
+        System.out.println(cum);
+
     }
 }
